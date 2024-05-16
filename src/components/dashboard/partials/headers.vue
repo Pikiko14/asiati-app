0adeb39824bbf93e49f65f44429b04628e912111
olasanmi <maalhequi95@gmail.com>
2024-05-16 13:30:29 -0500

    add search methods to header component

--- src/components/dashboard/partials/headers.vue
+++ src/components/dashboard/partials/headers.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="row">
+  <section class="row q-mt-md">
     <div class="col-12 col-md-6">
       <span class="text-primary text-h5 text-weight-semi-bold">
         {{ title }}
@@ -7,9 +7,16 @@
     </div>
     <div class="col-12 col-md-6">
       <section class="full-width row">
-        <div class="col-12 col-md-8">
+        <div class="col-12 col-md-8" :class="{ 'q-pr-sm': $q.screen.gt.sm }">
+          <q-input clearable @update:model-value="doSearch" debounce="1500" dense v-model="search"
+            placeholder="Buscar..." outlined rounded
+            :rules="[val => /^(?:[^,'']+)?$/.test(val) || 'El texto es invalido']">
+            <template v-slot:prepend>
+              <q-icon size="12pt" name="img:images/search.svg"></q-icon>
+            </template>
+          </q-input>
         </div>
-        <div class="col-12 col-md-4">
+        <div class="col-12 col-md-4" :class="{ 'q-pl-sm': $q.screen.gt.sm }">
           <q-btn @click="openModal" rounded size="12pt" :label="btnLabel" no-caps unelevated color="primary"
             class="full-width"></q-btn>
         </div>
@@ -19,7 +26,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { defineComponent, onBeforeMount, ref } from 'vue'
 
 export default defineComponent({
   name: 'HeaderComponent',
@@ -28,17 +36,37 @@ export default defineComponent({
     btnLabel: String
   },
   emits: [
-    'open-modal'
+    'open-modal',
+    'do-search'
   ],
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   setup(props, { emit }) {
+    // references
+    const route = useRoute()
+    const router = useRouter()
+    const search = ref<string>('')
+
+    const doSearch = () => {
+      const page = 1
+      const perPage = route.query.perPage || 12
+      const searchString = search.value
+      router.push({ query: { page, perPage, search: searchString } })
+    }
+
     // methods
     const openModal = () => {
       emit('open-modal')
     }
 
+    // lifecycles
+    onBeforeMount(() => {
+      search.value = route.query.search as string
+    })
+
     // return
     return {
+      search,
+      doSearch,
       openModal
     }
   }
