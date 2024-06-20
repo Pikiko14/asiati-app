87c15ace54df333b4188ed8e8da30251d512bf70
olasanmi <maalhequi95@gmail.com>
2024-06-20 09:53:41 -0500

    add build script

--- dist/spa/assets/notification.42b47c46.js
+++
@@ -1,1 +0,0 @@
-import{N as i}from"./index.f19e594f.js";const a=(o,t,e)=>{i.create({message:t,type:o,color:e})};export{a as notification};
--- dist/spa/assets/scopeValidation.5004ba91.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index.f19e594f.js";import{u as s}from"./auth.ce35a589.js";import"./notification.42b47c46.js";var m=t(async({app:o})=>{o.config.globalProperties.$hasPermission=a});const e=s(),a=o=>{const{scopes:r}=e.user;return!!(r!=null&&r.includes(o))};export{m as default};
